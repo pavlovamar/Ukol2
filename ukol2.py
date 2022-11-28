8e049f848d0fd1be9e494b2f453777b86396d1a0
Martina Pavlová <martina.pavlova00@seznam.cz>
2022-11-28 13:13:03 +0100

    Načtení vstupního souboru

--- ukol2.py
+++ ukol2.py
@@ -1,1 +1,7 @@
 # Napište program, který načte historická data o průměrných denních průtocích a spočítá roční a sedmidenní průměry. Program bude neinteraktivní.
+
+import csv
+
+with open("vstup.csv", encoding = "utf-8", newline = '') as f:
+    with open("vstup_7dni.csv", "w", encoding = "utf-8") as g:
+
