29291f0220e4c43690e9dc661e3a28b8ae138111
Martina Pavlová <martina.pavlova00@seznam.cz>
2022-12-03 10:53:36 +0100

    Zapsání dat do nového souboru

--- ukol2.py
+++ ukol2.py
@@ -5,5 +5,10 @@ import csv
 with open("vstup.csv", encoding = "utf-8", newline = '') as f:
     with open("vstup_7dni.csv", "w", encoding = "utf-8") as g:
         reader = csv.reader(f,delimiter=",")
+        next(reader)
+        writer = csv.writer(g)
         for row in reader:
-            print(row)
+            datum = row[-2]
+            prutok = row[-1]
+            outrow = [datum, prutok]
+            writer.writerow(outrow)
