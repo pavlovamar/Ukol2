2d06b1cadcf42d0215e30c11ec37543318131e60
Martina Pavlová <martina.pavlova00@seznam.cz>
2022-12-06 13:18:55 +0100

    Průměrný průtok, min a max

--- ukol2.py
+++ ukol2.py
@@ -3,18 +3,34 @@
 import csv
 
 with open("vstup.csv", encoding = "utf-8", newline = '') as f:
-    with open("vstup_7dni.csv", "w", encoding = "utf-8") as g:
+    with open("vystup_7dni.csv", "w", encoding = "utf-8") as g:
         reader = csv.reader(f,delimiter=",")
         writer = csv.writer(g)
         sum = 0
+        cislo_radku = 0
+        max_prutok = 0
+        min_prutok = 1000000
         for row in reader:
+            cislo_radku += 1
+            if float(row[-1]) > max_prutok:
+                max_prutok = float(row[-1])
+                max_den = row[-2]
+            if float(row[-1]) < min_prutok:
+                min_prutok = float(row[-1])
+                min_den = row[-2]
             try:
                 sum += float(row[-1])
             except ValueError:
                 pass
-            print(sum)
-            vysledny_prutok = round(sum, 4)
-            datum = row[-2]
-            prutok = row[-1]
-            outrow = [datum, prutok, vysledny_prutok]
-            writer.writerow(outrow)
+            if cislo_radku%7 == 1:
+                outrow = [row[0], row[1], row[2]]
+                writer.writerow(outrow)
+            if cislo_radku%7 == 0:
+                prumerny_prutok = round(sum/7, 4)
+                outrow = [prumerny_prutok]
+                writer.writerow(outrow)
+                sum = 0
+                cislo_radku = 0
+        print("Nejvyšší hodnota průtoku je " + str(max_prutok) + " a byla naměřena dne " + str(max_den))
+        print("Nejnižsí hodnota průtoku je " + str(min_prutok) + " a byla naměřena dne " + str(min_den))
+
