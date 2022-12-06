ad026c2583b65e1b46de51676667d6319f2b6d61
Martina Pavlová <martina.pavlova00@seznam.cz>
2022-12-06 14:38:37 +0100

    Komentáře

--- ukol2.py
+++ ukol2.py
@@ -1,32 +1,30 @@
-# Napište program, který načte historická data o průměrných denních průtocích a spočítá roční a sedmidenní průměry. Program bude neinteraktivní.
-
 import csv
-
+from math import inf
 with open("vstup.csv", encoding = "utf-8", newline = '') as f:
     with open("vystup_7dni.csv", "w", encoding = "utf-8") as g:
         reader = csv.reader(f,delimiter=",")
         writer = csv.writer(g)
-        sum = 0
+        prutok = 0
         cislo_radku = 0
         max_prutok = 0
-        min_prutok = 1000000
+        min_prutok = inf
         for row in reader:
-            cislo_radku += 1
-            if float(row[-1]) > max_prutok:
+            cislo_radku += 1                                    #očíslování řádků
+            if float(row[-1]) > max_prutok:                     #nejvyšší hodnota průtoku
                 max_prutok = float(row[-1])
                 max_den = row[-2]
-            if float(row[-1]) < min_prutok:
+            if float(row[-1]) < min_prutok:                     #nejnižší hodnota průtoku
                 min_prutok = float(row[-1])
                 min_den = row[-2]
             try:
-                sum += float(row[-1])
+                prutok += float(row[-1])                        #celkový průtok
             except ValueError:
                 pass
-            if cislo_radku%7 == 1:
+            if cislo_radku%7 == 1:                              #vypsání každého prvního dne v průměrovaném úseku
                 outrow = [row[0], row[1], row[2]]
                 writer.writerow(outrow)
-            if cislo_radku%7 == 0:
-                prumerny_prutok = round(sum/7, 4)
+            if cislo_radku%7 == 0:                              #vypsání průměrného průtoku za sedm dní
+                prumerny_prutok = round(prutok/7, 4)
                 outrow = [prumerny_prutok]
                 writer.writerow(outrow)
                 sum = 0
